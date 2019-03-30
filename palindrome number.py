2692f5b88b7564eb13b47090bf00deff6f08657c
c2lyh <c2lyh@outlook.com>
2019-03-30 11:02:15 -0700

    wtf is overwritten

---
+++ palindrome number.py
@@ -0,0 +1,10 @@
+
+def isPalindrome(x)->bool:
+
+	return ((str(x))[::-1] == str(x))
+	
+
+
+a = 121
+isPalindrome(121)
+
---
+++ reverseinteger.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+    	array = []
+    	f = False
+    	if x<0:
+    		x
+
+
+
+        
+
+
+if __name__ == '__main__':
+	b = 321
+	a = Solution()
+	a.reverse(b)
