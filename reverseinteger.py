7fc647486c2eface3bca546443ec92dddd09fbcc
c2lyh <c2lyh@outlook.com>
2019-03-30 11:50:24 -0700

    solved 32-bit range

--- reverseinteger.py
+++ reverseinteger.py
@@ -1,16 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
-    	array = []
-    	f = False
-    	if x<0:
-    		x
+    	if (x<0):
+    		y=-1*int((str(-x))[::-1])
+    	else:
+    		y=int((str(x))[::-1])
+    	if (y>(2**32/2-1) or y<(-(2**32/2))):
+    		y=0
+    	return y
 
 
-
-        
 
 
 if __name__ == '__main__':
-	b = 321
+	b = -321
 	a = Solution()
-	a.reverse(b)
+	print(a.reverse(b))
+	
